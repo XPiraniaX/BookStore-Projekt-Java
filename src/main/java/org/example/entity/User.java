354f3947f26d7a69591db5900f1cf67de72f8d40
Kamil Gębala <kamilgebala323@gmail.com>
2025-05-20 20:48:01 +0200

    User entity implementation and addition of requaired dependencies

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,39 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column()
+    private String password;
+
+    @Column()
+    private String email;
+
+    @Column()
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER,
+        ADMIN
+    }
+}
