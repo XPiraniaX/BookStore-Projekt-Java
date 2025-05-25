286bc3c88212ae3ea0b0e5fb3d4e975027396f33
Kamil Gębala <kamilgebala323@gmail.com>
2025-05-25 20:20:09 +0200

    BookLoan entity implementation

---
+++ src/main/java/org/example/entity/BookLoan.java
@@ -0,0 +1,40 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "loans")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookLoan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column()
+    private LocalDateTime loanDate;
+
+    @Column()
+    private LocalDateTime dueDate;
+
+    private LocalDateTime returnDate;
+
+    @Column()
+    private boolean returned;
+}
